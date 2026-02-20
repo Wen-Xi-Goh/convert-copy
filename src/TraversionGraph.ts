1d7e463505d6afdf4a9a3c4030772db1b0e5f248
p2r3 <p2r3@p2r3.com>
2026-02-20 03:54:36 +0200

    clean up logging and remove unused checks in TraversionGraph

--- src/TraversionGraph.ts
+++ src/TraversionGraph.ts
@@ -42,10 +42,6 @@ export interface Edge {
 };
 
 export class TraversionGraph {
-    constructor(disableSafeChecks: boolean = false) {
-        this.disableSafeChecks = disableSafeChecks;
-    }
-    private disableSafeChecks: boolean;
     private handlers: FormatHandler[] = [];
     private nodes: Node[] = [];
     private edges: Edge[] = [];
@@ -306,40 +302,15 @@ export class TraversionGraph {
             }
             if (current.index === toIndex) {
                 // Return the path of handlers and formats to get from the input format to the output format
-                console.log(`Found path at iteration ${iterations} with cost ${current.cost}: ${current.path.map(p => p.handler.name + "(" + p.format.mime + ")").join(" -> ")}`);
-                if (!this.disableSafeChecks) {
-                    // HACK HACK HACK!!
-                    //   Converting image -> video -> audio loses all meaningful media.
-                    //   For now, we explicitly check for this case to avoid blocking Meyda.
-                    let found = false;
-                    for (let i = 0; i < current.path.length; i ++) {
-                        const curr = current.path[i];
-                        const next = current.path[i + 1];
-                        const last = current.path[i + 2];
-                        if (!curr || !next || !last) break;
-                        if (
-                            [curr.format.category].flat().includes("image")
-                            && [next.format.category].flat().includes("video")
-                            && [last.format.category].flat().includes("audio")
-                        ) {
-                            found = true;
-                            break;
-                        }
-                    }
-                    if (found) {
-                        console.log(`Skipping path ${current.path.map(p => p.format.mime).join(" → ")} due to complete loss of media.`);
-                        continue;
-                    }
-                    // END OF HACK HACK HACK!!
-                }
+                const logString = `${iterations} with cost ${current.cost.toFixed(3)}: ${current.path.map(p => p.handler.name + "(" + p.format.mime + ")").join(" → ")}`;
                 if (simpleMode || !to.handler || to.handler.name === current.path.at(-1)?.handler.name) {
-                    console.log(`Found path at iteration ${iterations} with cost ${current.cost}: ${current.path.map(p => p.handler.name + "(" + p.format.mime + ")").join(" -> ")}`);
+                    console.log(`Found path at iteration ${logString}`);
                     this.dispatchEvent("found", current.path);
                     yield current.path;
                     pathsFound++;
                 }
                 else {
-                    console.log(`Unvalid path at iteration ${iterations} with cost ${current.cost}: ${current.path.map(p => p.handler.name + "(" + p.format.mime + ")").join(" -> ")}`);
+                    console.log(`Unvalid path at iteration ${logString}`);
                     this.dispatchEvent("skipped", current.path);
                 }
                 continue;
